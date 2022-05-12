e9cc07219030cb3ef29faec44eaf04e84bbb1dba
Devin Wall <devin.wall@thekrazycouponlady.com>
2022-05-12 04:04:41 -0600

    Small change.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
+import { Vector2 } from "three";
 
 import "./App.css";
 import PlanetTile from "./components/PlanetTile/PlanetTile";
-import GlRender from "./components/PlanetTile/GlRender";
+import CelestialBodyRenderer from "./components/PlanetTile/CelestialBodyRenderer";
 
 import { useSignInWithGoogle, useUser, useSignOut } from "./lib/firebase";
+import BasePlanet from "./lib/pixelPlanets/bodies/planets/BasePlanet";
+import { Color } from "./lib/pixelPlanets/core";
 
 const socket = io("http://localhost:3001", {
   transports: ["websocket", "polling"],
@@ -44,6 +47,16 @@ function App() {
 
   const { signInWithPopup } = useSignInWithGoogle();
 
+  const planet = new BasePlanet(
+    12345,
+    {
+      color1: new Color(155, 158, 184),
+      color2: new Color(71, 97, 124),
+      color3: new Color(53, 57, 85),
+    },
+    { position: new Vector2(), intensity: 0.1 }
+  );
+
   return (
     <div className="App">
       <nav>
@@ -60,7 +73,7 @@ function App() {
       <header>
         <h1>Dyson</h1>
       </header>
-      <GlRender />
+      <CelestialBodyRenderer celestialBody={planet} />
       <div className="Planetview">
         <h3>Planets:</h3>
         <div className="Planetgrid">
--- src/components/PlanetTile/CelestialBodyRenderer.tsx
+++ src/components/PlanetTile/CelestialBodyRenderer.tsx
@@ -1,28 +1,30 @@
 import React, { useEffect, useRef } from "react";
-import {
-  Clock,
-  // Group,
-  Scene,
-  WebGLRenderer,
-  PerspectiveCamera,
-  Vector2,
-} from "three";
+import { Clock, Scene, WebGLRenderer, PerspectiveCamera } from "three";
 
-import BasePlanet from "../../lib/pixelPlanets/bodies/planets/BasePlanet";
-import { Color } from "../../lib/pixelPlanets/core";
+import type { CelestialBody } from "../../lib/pixelPlanets/core";
 
-const GlRender = () => {
+type CelestialBodyRendererProps = {
+  celestialBody: CelestialBody;
+};
+
+const CelestialBodyRendererP = ({
+  celestialBody,
+}: CelestialBodyRendererProps) => {
   const renderContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     let id: any = null;
-    let rendererElement: any = null
-    const containerElement = renderContainerRef?.current
+    let rendererElement: any = null;
+    const containerElement = renderContainerRef?.current;
 
     if (containerElement) {
       const aspect =
-      containerElement.clientWidth /
-      containerElement.clientHeight;
+        containerElement.clientWidth / containerElement.clientHeight;
+
+      const clock = new Clock();
+      const scene = new Scene();
+
+      scene.add(celestialBody.createNode());
 
       const renderer = new WebGLRenderer();
 
@@ -36,40 +38,26 @@ const GlRender = () => {
       const camera = new PerspectiveCamera(75, aspect, 0.1, 100000);
       camera.position.z = 1;
 
-      const clock = new Clock();
-      const scene = new Scene();
-      const planet = new BasePlanet(
-        12345,
-        {
-          color1: new Color(155, 158, 184),
-          color2: new Color(71, 97, 124),
-          color3: new Color(53, 57, 85),
-        },
-        { position: new Vector2(), intensity: 0.1 }
-      );
-
-      scene.add(planet.createNode());
-
       function animate() {
         id = window.requestAnimationFrame(animate);
 
-        planet.update(clock.getElapsedTime());
+        celestialBody.update(clock.getElapsedTime());
 
         //update camera
         camera.updateProjectionMatrix();
         renderer.render(scene, camera);
       }
 
-      rendererElement = renderer.domElement
+      rendererElement = renderer.domElement;
       renderContainerRef.current.appendChild(rendererElement);
       animate();
     }
 
     return () => {
-      rendererElement && containerElement?.removeChild(rendererElement)
+      rendererElement && containerElement?.removeChild(rendererElement);
       id && window.cancelAnimationFrame(id);
     };
-  }, [renderContainerRef]);
+  }, [renderContainerRef, celestialBody]);
 
   return (
     <div
@@ -80,4 +68,4 @@ const GlRender = () => {
   );
 };
 
-export default GlRender;
+export default CelestialBodyRendererP;
