4e892a45557d3bdc5216fad9d3c3ab595e63f17d
Devin Fahnestock <devinmfahnestock@gmail.com>
2022-08-10 17:49:20 -0400

    UserCard Refactoring Complete

---
+++ packages/frontend/src/components/NavBar/UserCard/UserCard.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+//import './styles.css'
+
+import { useSignOut } from '../../../lib/firebase'
+
+const UserCard = ({ user }: any) => {
+  const signOut = useSignOut()
+
+  return (
+    <div className='profile'>
+      <div className='profile name'>
+        {user.displayName}
+        <a
+          onClick={() => {
+            signOut()
+          }}
+        >
+          Sign out
+        </a>
+      </div>
+      <img src={user.photoURL!} alt='Profile' />
+    </div>
+  )
+}
+
+export default UserCard
---
+++ packages/frontend/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+      Create a new Account Username: <input className='usernameInput' />
+      Password: <input className='passwordInput' type='password' />
+    </div>
+  )
+}
+
+export default SignUp
