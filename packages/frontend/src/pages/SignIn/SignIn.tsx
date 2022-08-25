7c95a8942302c3770a4b5dc05e6589d180383f46
Devin Fahnestock <devinmfahnestock@gmail.com>
2022-08-25 17:09:00 -0400

    UserTokens working now

--- packages/frontend/src/pages/SignIn/SignIn.tsx
+++ packages/frontend/src/pages/SignIn/SignIn.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { useSignInWithGoogle } from 'src/lib/firebase'
 import './styles.css'
 
 const SignIn = () => {
+  const navigate = useNavigate()
   const { signInWithPopup } = useSignInWithGoogle()
   return (
     <div className='SigninForm'>
       SignIn
-      <button onClick={() => signInWithPopup()}>Sign In With Google</button>
+      <button
+        onClick={() => {
+          signInWithPopup().then(() => {
+            navigate('/')
+          })
+        }}
+      >
+        Sign In With Google
+      </button>
     </div>
   )
 }
