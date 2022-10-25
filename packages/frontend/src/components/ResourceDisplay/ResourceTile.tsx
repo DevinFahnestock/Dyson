c12f116e6eb06cbe1414ccfde5cb79ec209e4beb
Devin Fahnestock <devinmfahnestock@gmail.com>
2022-10-25 12:43:02 -0400

    added resource icons

--- packages/frontend/src/components/ResourceDisplay/ResourceTile.tsx
+++ packages/frontend/src/components/ResourceDisplay/ResourceTile.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './styles.css'
 
+import components from './resources'
+
 const ResourceTile = ({ type, amount }: any) => {
+  const Component = components[type]
+
   return (
     <div className='ResourceTile'>
+      <Component />
       {type}: {amount}
     </div>
   )
